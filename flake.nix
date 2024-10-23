77bd691465e154cc93d0f5f726184a0171ce3051
haruki7049 <tontonkirikiri@gmail.com>
2024-10-23 18:42:51 +0900

    Init

--- flake.nix
+++ flake.nix
@@ -47,6 +47,17 @@
             userhome-configs = import ./src/home/users/default.nix;
           };
         };
+
+        homeConfigurations = {
+          "haruki-x86_64-linux" = inputs.home-manager.lib.homeManagerConfiguration {
+            pkgs = import inputs.nixpkgs {
+              system = "x86_64-linux";
+              modules = [
+                ./src/home/users/haruki.nix
+              ];
+            };
+          };
+        };
       };
 
       perSystem =
