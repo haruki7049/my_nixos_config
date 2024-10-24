c128fb8276b336dc1822ee5636a0a5d14f86ed7e
haruki7049 <tontonkirikiri@gmail.com>
2024-10-24 11:45:31 +0900

    boo

--- src/systems/enmac/configuration.nix
+++ src/systems/enmac/configuration.nix
@@ -3,5 +3,14 @@
 {
   services.nix-daemon.enable = true;
 
+  programs = {
+    bash.enable = true;
+    zsh.enable = true;
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+  };
+
   system.stateVersion = 5;
 }
